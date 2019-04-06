a7ac6710943b86dcf5769e62aae7373e3524557d
emags112 <magnussen.evan@gmail.com>
2019-04-05 23:18:21 -0400

    initial commit

---
+++ src/components/banner.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+
+class MenuBtn extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      ...props
+    }
+  }
+  render(){
+    return (<a className="menubar__btn"href={this.state.href || "#"}>{this.state.text}</a>)
+  }
+}
+
+export class Banner extends Component{
+  render(){
+    return(<header className="banner">
+            <h1 className="banner__head">Evan Magnussen</h1>
+            <h2 className="banner__subhead">Web Developer - Runner - Cook - Guitarist </h2>
+          </header>)
+  }
+}
+
+export class Menubar extends Component{
+render(){
+  return(<div className="menubar">
+            <MenuBtn text="portfolio" />
+            <MenuBtn text="github" />
+            <MenuBtn text="codepen" />
+            <MenuBtn text="blog" />
+        </div>)
+}
+}
