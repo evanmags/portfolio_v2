0a937051ea2ec5dba4fcbfc0e7d3cbe783727650
emags112 <magnussen.evan@gmail.com>
2019-04-09 08:47:50 -0400

    blog card creator function

--- src/components/blog.js
+++ src/components/blog.js
@@ -5,13 +5,37 @@ class BlogPost extends Component{
     return <div></div>
   }
 }
-
+const cards = [
+  {title: "I ran Today",
+  description: "It was very windy but it was still a good day in my book. I did about 7 miles and it was faster than previous, similar runs. I felt ok about it but still want more.",
+  length: "250 words. 4 minutes",
+  date: "12/3/18"},
+  {title: "I ran Today",
+  description: "It was very windy but it was still a good day in my book. I did about 7 miles and it was faster than previous, similar runs. I felt ok about it but still want more.",
+  length: "250 words. 4 minutes",
+  date: "12/3/18"},
+  {title: "I ran Today",
+  description: "It was very windy but it was still a good day in my book. I did about 7 miles and it was faster than previous, similar runs. I felt ok about it but still want more.",
+  length: "250 words. 4 minutes",
+  date: "12/3/18"},
+  {title: "I ran Today",
+  description: "It was very windy but it was still a good day in my book. I did about 7 miles and it was faster than previous, similar runs. I felt ok about it but still want more.",
+  length: "250 words. 4 minutes",
+  date: "12/3/18"},
+]
 class BlogCard extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      ...props
+    }
+  }
   render(){
     return (<div className="blog__card">
-              <h2 className="blog__title">I ran Today</h2>
-              <p className="blog__description">It was very windy but it was still a good day in my book. I did about 7 miles and it was faster than previous, similar runs. I felt ok about it but still want more.</p>
-              <span className="blog__length">250 words. 4 minutes</span>
+              <h2 className="blog__title">{this.state.title}</h2>
+              <span className="blog__date">{this.state.date}</span>
+              <p className="blog__description">{this.state.desc}</p>
+              <span className="blog__length">{this.state.length}</span>
             </div>)
   }
 }
@@ -22,7 +46,7 @@ export default class Blog extends Component {
     this.state = {}
   }
   blog_cards(){
-    return <BlogCard />;
+    return cards.map(card => <BlogCard title={card.title} date={card.date} desc={card.description} length={card.length}/>)
     // make call to cms
     // get titles
     // get description
