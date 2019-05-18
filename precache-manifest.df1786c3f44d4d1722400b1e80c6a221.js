2f6d2b952251525e8a603d40b6f5c13178a307d5
emags112 <magnussen.evan@gmail.com>
2019-05-18 16:25:37 -0400

    Updates

---
+++ precache-manifest.df1786c3f44d4d1722400b1e80c6a221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "70e4d62da7af9b5f6a51",
+    "url": "/static/js/main.0bd404ae.chunk.js"
+  },
+  {
+    "revision": "3b07c86783909886443a",
+    "url": "/static/js/2.c08d0d59.chunk.js"
+  },
+  {
+    "revision": "70e4d62da7af9b5f6a51",
+    "url": "/static/css/main.bcdc8ce4.chunk.css"
+  },
+  {
+    "revision": "0ed78b1a449d473d38d361d99dfc87b9",
+    "url": "/index.html"
+  }
+];
