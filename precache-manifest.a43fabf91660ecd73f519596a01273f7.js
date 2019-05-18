6c3b94573cd4df2246f43f7eda4e3a45525e9b28
emags112 <magnussen.evan@gmail.com>
2019-05-18 14:29:05 -0400

    Updates

--- precache-manifest.a43fabf91660ecd73f519596a01273f7.js
+++ precache-manifest.a43fabf91660ecd73f519596a01273f7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio_v2/static/js/runtime~main.a7a80610.js"
   },
   {
-    "revision": "d25cb5723362b405fdfa",
-    "url": "/portfolio_v2/static/js/main.7a6e1203.chunk.js"
+    "revision": "4fda533f2a506f11d542",
+    "url": "/portfolio_v2/static/js/main.d11bfbfc.chunk.js"
   },
   {
     "revision": "c3057867d87d10578f59",
     "url": "/portfolio_v2/static/js/2.d8f8affe.chunk.js"
   },
   {
-    "revision": "d25cb5723362b405fdfa",
-    "url": "/portfolio_v2/static/css/main.ad6e6fca.chunk.css"
+    "revision": "4fda533f2a506f11d542",
+    "url": "/portfolio_v2/static/css/main.888f0b95.chunk.css"
   },
   {
-    "revision": "b120ad6e1e8723f2e8372c526144f6ee",
+    "revision": "b607a8dde50a0ae872cdda80c4a30ce2",
     "url": "/portfolio_v2/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio_v2/precache-manifest.e1bb4d9d327a496b17dcf345907c8e0e.js"
+  "/portfolio_v2/precache-manifest.a43fabf91660ecd73f519596a01273f7.js"
 );
 
 workbox.clientsClaim();
