13035b577f0899e66aefeeb7b900b36972248094
emags112 <magnussen.evan@gmail.com>
2019-05-19 10:43:23 -0400

    fix footer for accessability in lighthouse

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ function Contact() {
   return require('../data.json').resume.contact.map(element => {
     if(element.type === "phone") return false;
     return (
-        <a className="footer__link" href={element.href}>
-          <i className={element.icon} />
+        <a id={element.type} className="footer__link" href={element.href}>
+          <i className={element.icon} title={element.type}/>
         </a>
     );
   });
